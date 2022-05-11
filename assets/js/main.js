828f8103a4ee5a01c27996afbb955248bec38f86
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-11 16:31:18 +0200

    erste Aufgabe

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+console.log('Grias di!');
+
+
+// Level1_1
+
+let naMe = " Jane ";
+let nachName = " S ";
+let alter = 30;
+let geburtsOrt = " Schwobminga ";
+let grosse = " 1,65cm ";
+let gewicht = " immer anders vui";
+let hobbies = " Musi ";
+let computer = " Codecombat ";
+let essen = " ois von Renate ";
+let sport = " Lafa ";
+let modemarke = " brach i ned ";
+let jahreszeit = " olle ";
+let Urlaubsort = " Fliaga";
+
+
+let result = `I hoaß ${naMe} ${nachName}, bin ${alter}, komm vo ${geburtsOrt} und bin ${grosse}.I wiag ${gewicht}, mog ${hobbies}, spui ${computer}, iss ${essen}, bin moargnds ${sport}, Marka ${modemarke}, mog ${jahreszeit} Zeita vom Joahr und mei Bua wuill ${Urlaubsort}.`;
+
+One = document.getElementById('ten');
+One.innerHTMl = result;
+console.log(result);
+
+// 
